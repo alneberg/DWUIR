3eb58fa3d37e51f01cbb9f5476954c56e90c073c
Johannes Alneberg <alneberg@kth.se>
2019-04-11 13:42:17 +0200

    Preprocessing test actually runs the script

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,18 +1,33 @@
+import subprocess
+import os
+
 def run_preprocessing():
-    return "dummy"
+    testdir = os.path.dirname(os.path.realpath(__file__))
+    input_file = os.path.join(testdir, 'test_data', '1_read.fastq.gz')
+    script_path = os.path.join(testdir, '..', 'dwuir', 'DWUIR_preprocessing.py')
+    return subprocess.Popen(['python', script_path, input_file], stdout=subprocess.PIPE).stdout
 
 def test_umi_placement():
     for line in run_preprocessing():
-        print(line)
-        assert line.split(':')[7].split(' ')[0] == 'CATGACGAC'
+        assert line.split(b':')[7].split(b' ')[0] == b'CATGACGAC'
         # Test only header line, so break here
         break
 
 def test_index_header_placement():
     for line in run_preprocessing():
-        assert line.split(':')[-1] == "CACCCGTT+GCTATCCT"
+        assert line.split(b':')[-1] == b"CACCCGTT+GCTATCCT"
 
+def test_read_sequence_R1():
+    first = True
+    second = False
+    for line in run_preprocessing():
+        if first:
+            first = False
+        else: # Only test the second line
+            assert line.startswith(b'CACCGTT')
+            second = True
+            break
+    assert second # Make sure the else case is executed
 
 if __name__ == '__main__':
-    test_umi_placement()
-    test_index_header_placement()
+    pass
