a4a9fccaebb50ff30b765cf143074aab7f7d2d76
Johannes Alneberg <alneberg@kth.se>
2019-04-11 14:28:01 +0200

    Added tests for fake r2 for single end sequencing

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -8,16 +8,19 @@ def run_preprocessing():
     return subprocess.Popen(['python', script_path, input_file], stdout=subprocess.PIPE).stdout
 
 def test_umi_placement():
+    """Correct UMI in R1"""
     for line in run_preprocessing():
         assert line.split(b':')[7].split(b' ')[0] == b'CATGACGAC'
         # Test only header line, so break here
         break
 
 def test_index_header_placement():
+    """Correct index in header of R1"""
     for line in run_preprocessing():
         assert line.split(b':')[-1] == b"CACCCGTT+GCTATCCT"
 
 def test_read_sequence_R1():
+    """First index placed in R1"""
     first = True
     second = False
     for line in run_preprocessing():
@@ -28,6 +31,21 @@ def test_read_sequence_R1():
             second = True
             break
     assert second # Make sure the else case is executed
+
+def test_interleaved():
+    """Exactly 2 interleaved reads"""
+    output = run_preprocessing().readlines()
+    assert len(output) == 8
+
+def test_r1r2_header():
+    """Identical header except R1/R2 denotion"""
+    output = run_preprocessing().readlines()
+    assert output[4].replace(b' 2:N', b' 1:N') == output[0]
+
+def test_r2_index():
+    """Test insertion of index in R2"""
+    output = run_preprocessing().readlines()
+    assert output[5].startswith('GCTATCCT')
 
 if __name__ == '__main__':
     pass
